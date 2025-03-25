15759056b1f6b045578173bb892b2ad7573b9fdc
Audrey PEHUET <audrey.pehuet@gmail.com>
2025-03-25 11:53:14 +0100

    Création authentification/regis form/ navb

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,6 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Participant;
+use App\Entity\Site;
+use App\Entity\Sortie;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -17,6 +20,14 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('email')
+            ->add('nom')
+            ->add('prenom')
+            ->add('telephone')
+            ->add('site', EntityType::class, [
+                'class' => Site::class,
+                'choice_label' => 'id',
+            ])
             ->add('email')
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
