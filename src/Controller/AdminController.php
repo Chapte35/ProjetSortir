2636d81dbda353c46cffed3e5cbdc3bd25b7050c
Sway1240 <oaitlahssaine@gmail.com>
2025-03-30 19:05:42 +0200

    Admin peux desactiver/supprimer les participants (pas les a admin). les compte inactif peux pas se connecter.

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Participant;
 use App\Entity\Sortie;
 use App\Form\UploadCsvType;
+use App\Repository\ParticipantRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -63,8 +64,47 @@ final class AdminController extends AbstractController
         }
 
         return $this->render('admin/upload.html.twig', [
+
             'form' => $form->createView(),
         ]);
     }
+
+
+    #[Route('/userlist', name: 'app_admin_userlist')]
+    public function userlist(ParticipantRepository $participantRepository): Response
+    {
+        $users = $participantRepository->findAll();
+        $users = array_filter($users, function ($user) {
+            return !in_array('ROLE_ADMIN', $user->getRoles());
+        });
+        return $this->render('admin/userlist.html.twig', [
+            'users' => $users,
+        ]);
+    }
+
+    #[Route('/desactiver/{id}', name: 'app_admin_desactiver')]
+    public function desactiver(ParticipantRepository $participantRepository,EntityManagerInterface $entityManager,int $id): Response
+    {
+
+        $participants = $participantRepository->find($id);
+        $user = $participants->setActif(false);
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('admin_app_admin_userlist');
+    }
+
+    #[Route('/supprimer/{id}', name: 'app_admin_supprimer')]
+    public function supprimer(ParticipantRepository $participantRepository,EntityManagerInterface $entityManager,int $id): Response
+    {
+
+        $participants = $participantRepository->find($id);
+        $entityManager->remove($participants);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('admin_app_admin_userlist');
+    }
+
+
 }
 
